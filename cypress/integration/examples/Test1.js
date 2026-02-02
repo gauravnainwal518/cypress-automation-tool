9cd56e57ea03b5ef12b8c78271d500f76801e610
gauravnainwal229 <gauravnainwal229@gmail.com>
2026-02-03 01:19:25 +0530

    feat(cypress): add element selection, async behavior, aliases, and logging
    - Continued automation after initial Cypress setup
    - Practiced cy.get() element finding and chaining
    - Explored async vs sync execution in Cypress commands
    - Implemented aliases using .as() and cy.get('@alias')
    - Added logging via cy.log() and Cypress test runner

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,7 @@
 //cypress - Specs
 
-//// <reference types = "Cypress"/>
+/// <reference types="cypress" />
+
 describe('My first Test Suite', function()
 {
 
@@ -9,7 +10,36 @@ it("My firstTest case", function(){
 cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
   
 cy.get('.search-keyword').type('ca')
+cy.get('.search-keyword')
+cy.wait(2000)
+// visible method 
+cy.get('.product:visible').should('have.length', 4)
+
+//parent child chaining get and find methods and we select product based on index
+cy.get('.products').as ('productLocator')
+cy.get('@productLocator').find('.product').should('have.length', 4)
+cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
+
+//now we selecting text dynamically directly not selecting index 
+//so we used each method
+
+cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+ const textVeg =  $el.find('h4.product-name').text()
+ if(textVeg.includes('Cashews')){
+ cy.wrap($el).find('button').click()
+ }
 })
-cy.get('.product')
+//assert if logo text is correctly displayed
+cy.get('.brand').should('have.text','GREENKART')
+ 
+//this is the print in logs
+cy.get('.brand').then(function(logoelement)
+ {
+cy.log(logoelement.text())
+ })
+
+})
+ 
+ 
 
 } )
