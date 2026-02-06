04a4d63c3fec7c17cd32642ad15bc55002b822b3
gauravnainwal229 <gauravnainwal229@gmail.com>
2026-02-06 20:29:33 +0530

    add comment for future reference

--- cypress/integration/examples/Test7.js
+++ cypress/integration/examples/Test7.js
@@ -5,6 +5,9 @@ describe("Handling child windows", function(){
   it("should have child windows", function(){
 
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+    // This test removes target="_blank" so the link opens in the same tab.
+// It simulates real user behavior by clicking the link and validating the new page.
+
     
     cy.get('#opentab').invoke('removeAttr','target').click() 
    // cy.get('#opentab').click() //if we didnot give above jquery function so its open on another tab 
