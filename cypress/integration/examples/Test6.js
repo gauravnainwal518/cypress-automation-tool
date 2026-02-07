662a89a17c82231e67803ac4d2e52bd67a12b67f
gauravnainwal229 <gauravnainwal229@gmail.com>
2026-02-07 20:04:37 +0530

    add comments

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -1,12 +1,21 @@
 /// <reference types = 'cypress' /> 
 
-describe("this is my sixth test ", ()=>{
-  it("this is my sixth testsuit", ()=>{
-  //handling mousehover on invisible elements 
+describe("Sixth Test - Mouse Hover Handling", () => {
 
+  it("should handle mouse hover and click hidden element", () => {
+
+    // Visit the practice automation website
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-    //cy.get('div.mouse-hover-content').invoke('show'); //first method using jquery show ()
-    cy.contains('Top').click({force: true}) //second method is {force: true} for clicking only 
+
+    // The 'Top' link is inside a hidden hover menu.
+    // Method 1 (using jQuery): make the hidden element visible
+    // cy.get('div.mouse-hover-content').invoke('show')
+
+    // Method 2: directly click the hidden element using force: true
+    // This bypasses Cypress visibility checks
+    cy.contains('Top').click({ force: true })
+
+    // Validate that the URL now includes 'top'
     cy.url().should('include', 'top')
   })
 })
