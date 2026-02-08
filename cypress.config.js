e8efdace5ba0ac86cea2a91dd2c789dcd51e3b67
gauravnainwal229 <gauravnainwal229@gmail.com>
2026-02-08 22:02:49 +0530

    adding new file test11.js that hold the previous code without applying framework and afther that we start with test11 framework.js and design objects and execute the code and also see cypress custom commands in commands.js folder and how to use environment variable(env)

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 6000, //i am overwriting the default value that is 4000 in cypress now its take 6 second for failed any test case 
+  env:{
+   url: "https://rahulshettyacademy.com"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/integration/examples/Test11Framework.js
+++ cypress/integration/examples/Test11Framework.js
@@ -1,54 +1,74 @@
-describe("End to End Ecommerce Test", () => {
+import HomePage from '../../support/pageObjects/HomePage'
 
-  before(function () {
-    // runs once before all tests
-    cy.fixture("example").then(function (data) {
-      this.data = data
-    })
-  })
+describe('End to End ecommerce Test', function () {
 
-  it("Submit Order", function () {   
-    const productName = this.data.productName
-    
-   cypress.config('defaultCommandTimeout', 10000) //now this test cas only take 10 second 
-   const homepage = new HomePage()
-   homepage.goTo("https://rahulshettyacademy.com/loginpagePractise/#")
-    
-     
-     homepage.login(this.data.username, this.data.password)
-    
-    cy.contains("Shop Name").should('be.visible')
-    cy.get('app-card').should('have.length', 4)
+    before(function(){
+        //runs once before all tests in this block
+        cy.fixture('example').then(function(data)
+        {
+            this.data=data
+            this.homepage = new HomePage()
+        })
 
-    // dynamically selecting card
-    cy.get('app-card').filter(`:contains("${productName}")`)
-      .then($element => {
-        cy.wrap($element).should('have.length', 1)
-        cy.wrap($element).contains('button', 'Add').click()
-      })
-
-    // select by index
-    cy.get('app-card').eq(0).contains('button', 'Add').click()
-
-    // checkout
-    cy.contains('a', 'Checkout').click()
-
-    let sum = 0
-    cy.get('tr td:nth-child(4) strong')
-      .each($el => {
-        const amount = Number($el.text().split(" ")[1].trim())
-        sum = sum + amount
-      })
-      .then(function () {
-        expect(sum).to.be.lessThan(200000)
-      })
-
-    cy.contains('button', 'Checkout').click()
-    cy.get('#country').type('India')
-    
-    cy.get('.suggestions ul li a').click()
-
-    cy.get('.btn-success').click()
-    cy.get('.alert-success').should('contain', 'Success')
-  })
 })
+
+
+    it('Submit Order', function () {
+       
+        const productName = this.data.productName
+         
+        this.homepage.goTo(Cypress.env('url')+"/loginpagePractise/")     
+        const productPage = this.homepage.login(this.data.username,this.data.password)     
+        productPage.pageValidation()
+        productPage.getCardCount().should('have.length', 4)
+        productPage.selectProduct(productName)
+        productPage.selectFirstProduct()
+        const cartPage = productPage.goToCart()
+        cartPage.sumOfProducts().then(function(sum)
+    {
+        expect(sum).to.be.lessThan(200000);
+    })
+        const confirmationPage = cartPage.checkoutItems()
+        confirmationPage.submitFormDetails()
+        confirmationPage.getAlertMessage().should('contain','Success')
+
+
+
+       
+     
+        
+       
+ 
+          
+
+
+
+
+    });
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
