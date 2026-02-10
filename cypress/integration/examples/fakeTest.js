d6d392187a4dad5116240899a7a4b3de34e60beb
gauravnainwal229 <gauravnainwal229@gmail.com>
2026-02-10 16:45:37 +0530

    Integration Testing with Front end and Back End response validation assertions

--- cypress/integration/examples/fakeTest.js
+++ cypress/integration/examples/fakeTest.js
@@ -1,34 +1,36 @@
-/// <reference types = 'cypress' />
+/// <reference types='cypress' />
 
-describe("mocking http request", function(){
-  it("mocking the http request", function(){
+describe("mocking http request", function () {
+  it("mocking the http request", function () {
 
-    cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
-cy.intercept({
-  method: 'GET',
-  url:  'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+    cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
 
-},
-  
-{
-  statusCode: 200,
-  body: [
-    {
-      "book_name": "RestAssured with Java",
-      "isbn": "RSU",
-      "aisle": "2301"
-    }
-  ]
-}).as('bookretrievals')
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '**/Library/GetBook.php?AuthorName=shetty',
+      },
+      {
+        statusCode: 200,
+        body: [
+          {
+            book_name: "RestAssured with Java",
+            isbn: "RSU",
+            aisle: "2301"
+          }
+        ]
+      }
+    ).as('bookretrievals');
 
-cy.get("button[class = 'btn btn-primary']").click()
-cy.wait('@bookretrievals')
-cy.get('p').should('have.text','Sorry we have only one book available')
+    cy.get('button.btn.btn-primary').click();
 
+    cy.wait('@bookretrievals')
+      .its('response.body')
+      .then((body) => {
+        cy.get('tr').should('have.length', body.length + 1);
+      });
 
-
-
-
-
-  })
-})
+    cy.get('p')
+      .should('have.text', 'Sorry we have only one book available');
+  });
+});
